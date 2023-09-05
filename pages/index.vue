b1e88c12abf17119c84521ee91ac81c1f3d37e92
DinhTX <dinhtx@pascaliaasia.com>
2023-09-05 16:37:04 +0700

    Init code

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Test</p>
+  </div>
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
+
+<style lang="scss" scoped>
+  div {
+    font-size: 30px;
+    padding: 100px;
+  }
+</style>
